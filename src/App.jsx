e50941bf1356a4151a58e577a217100f4bf0fde8
Nitin Ahuja <53057442+ahnitin@users.noreply.github.com>
2024-02-18 11:04:54 +0530

    Building the header of your restaurant app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Header from "./components/Layout/Header";
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+};
+export default App;
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,73 @@
+import styles from "./Header.module.css";
+import { IoIosCart } from "react-icons/io";
+const Header = () => {
+  return (
+    <header className={`p-3 mb-3 border-bottom ${styles["header"]}`}>
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none"
+          >
+            <svg
+              className="bi me-2"
+              width="40"
+              height="32"
+              role="img"
+              aria-label="Bootstrap"
+            >
+              <use xlink:href="#bootstrap"></use>
+            </svg>
+          </a>
+
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <a href="#" className="nav-link px-2 link-secondary">
+                Overview
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-body-emphasis">
+                Inventory
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-body-emphasis">
+                Customers
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-body-emphasis">
+                Products
+              </a>
+            </li>
+          </ul>
+
+          <form
+            className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
+            role="search"
+          >
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Search..."
+              aria-label="Search"
+              data-listener-added_7224880e="true"
+            />
+          </form>
+          <button
+            type="button"
+            className={`btn btn-danger position-relative ${styles.cart}`}
+          >
+            <IoIosCart />
+            <span> Your Cart </span>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              0
+            </span>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
